827737045b61f2de2ec56e5bb674c018a4de8897
Tmeng <tonymengt@gmail.com>
2022-11-30 18:00:55 -0500

    incorporated global state for category and product list component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
+import { StoreProvider } from "./utils/GlobalState";
+
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
@@ -40,6 +42,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
+        <StoreProvider>
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
@@ -49,6 +52,7 @@ function App() {
             <Route exact path="/products/:id" component={Detail} />
             <Route component={NoMatch} />
           </Switch>
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,24 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
+import { useStoreContext } from "../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../utils/actions";
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
+  // const { id } = useParams();
+
+  // const [currentProduct, setCurrentProduct] = useState({});
+
+  // const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  // const products = data?.products || [];
+
+  // useEffect(() => {
+  //   if (products.length) {
+  //     setCurrentProduct(products.find((product) => product._id === id));
+  //   }
+  // }, [products, id]);
+
+  const [state, dispatch] = useStoreContext();
   const { id } = useParams();
-
-  const [currentProduct, setCurrentProduct] = useState({});
-
+  
+  const [currentProduct, setCurrentProduct] = useState({})
+  
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-
-  const products = data?.products || [];
-
+  
+  const { products } = state;
+  
   useEffect(() => {
     if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
+      setCurrentProduct(products.find(product => product._id === id));
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
     }
-  }, [products, id]);
+  }, [products, data, dispatch, id]);
 
   return (
     <>
